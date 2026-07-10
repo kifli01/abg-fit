0d8129f3e0f5f0c0e50633d8ac5ffd287b73c01b
kifli01 <attila@abg.hu>
2026-07-10 14:49:11 +0200

    feat: add isAdmin flag to AuthState

--- src/features/auth/types.ts
+++ src/features/auth/types.ts
@@ -4,6 +4,7 @@ export interface AuthState {
   user: User | null;
   isLoading: boolean;
   isAuthorized: boolean;
+  isAdmin: boolean;
   authError: string | null;
   signInWithGoogle: () => Promise<void>;
   signOut: () => Promise<void>;
